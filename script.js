2fda480e4f0181d85e71bc963ffc9d8dc5669678
XDobson <xander.shaffer@stu.caverna.kyschools.us>
2022-02-11 14:05:41 -0600

    now we know java woooooo

---
+++ script.js
@@ -0,0 +1,23 @@
+/*
+1. Use a console log to have your favorite number display in the console/terminal
+2. Use a comment to type out the 5 main data types (AKA Primitives)
+3. Use a comment to type out the 6 operator symbols
+4. Use modulo to get a remainder of 1 from the number 5. (There are two answers for this one. Please provide both. Also, please type them both out. Do not just type out the two numbers)
+5. Using single quotes at the start and end of the string have the words I'M MARY POPPINS, Y'ALL! display in the terminal/console (Must use a console log for this one.)
+6. Using Concatenation combine the following strings into one string. First string: "Hawk". Second string: "eye".
+7. Create a string for your first name and also your last name. Use the length property to add them together (Answer should be a number)
+8. Use index on the following string to select the lowercase "e" at the end of the string. "Avengers: Endgame". (Use a console log to display it in the console/terminal)
+*/
+
+console.log(68);
+// Numbers, Strings, Boolean, Null, Undefined
+//  + - * / %
+mod1 = 5 % -4
+mod2 = 5 % 4
+mod3 = 1 % 5
+mod4 = 6 % 5
+console.log(mod1,mod2,mod3,mod4)
+console.log('I\'M MARRY POPPINS, Y\'ALL')
+console.log("Hawk"+"eye")
+console.log("Xander".length+"Dobson".length)
+console.log("Avengers: Endgame"[16])
